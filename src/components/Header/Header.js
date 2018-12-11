aa8a58541cbadc9a2357b380318a28209ab00c88
David Watson <mdwat90@gmail.com>
2018-12-11 12:32:20 -0700

    Updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <h3 className="navbar-brand">Click Game</h3>
+                <h3>Click Game</h3>
                 <div className="collapse navbar-collapse" id="navbarText">
                     <span className="navbar-text ml-auto">
                         <p>Current Score: {this.props.currentScore}</p>
